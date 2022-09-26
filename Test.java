a351f2ec1161a55e3d493ba113e9db9224dc8383
ikjunoob <dlrwn0730@naver.com>
2022-09-26 10:27:54 +0900

    first commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        print();
+        System.out.println("생활코딩");
+    }
+    static void print(){
+        System.out.println("나는 익주");
+    }
+}
---
+++ free.kt
@@ -0,0 +1,4 @@
+fun main(){
+    val a = 1
+    var b = 1
+}
