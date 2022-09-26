30ec46c8b2bf4d71359eef812e391621413686c6
ikjunoob <dlrwn0730@naver.com>
2022-09-26 10:36:15 +0900

    문자열 입출력

--- Test.java
+++ Test.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
-        print();
-        System.out.println("생활코딩");
-    }
-    static void print(){
-        System.out.println("나는 익주");
+        String a; String b;
+        Scanner ikju = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력하세오:");
+        a = ikju.next();
+        System.out.println("두 번째 숫자를 입력하세오:");
+        b = ikju.next();
+        System.out.println(a+b);
     }
 }
